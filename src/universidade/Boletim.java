7b731969fa7770d54ddf8381e34472399e949d53
Pietro Medeiros <spogmedeiros@gmail.com>
2021-11-03 18:59:19 -0300

    fix

--- src/universidade/Boletim.java
+++ src/universidade/Boletim.java
@@ -1,13 +1,10 @@
 package universidade;
 
 public class Boletim {
+	//atributos
 	private Integer id_aluno;
 	private Integer id_curso;
 	private Double notaP1;
 	private Double notaP2;
 	private Double notaP3;
-	
-	
-	
-    
 }
--- src/universidade/Curso.java
+++ src/universidade/Curso.java
@@ -1,9 +1,9 @@
 package universidade;
 
 public class Curso {
+	//atributos
 	private String nome;
 	private Integer id_curso;
 	private Integer id_aluno;
 	private Integer id_professor;
-
 }
